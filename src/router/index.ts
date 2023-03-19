45204a10f67b0d799e4b450bbb9fcac2dc4ea1d1
Aram479 <1627474159@qq.com>
2023-03-19 15:54:21 +0800

    添加vercel线上配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Login from "@/views/Login/Login.vue";
 import Layout from "@/views/Layout/Layout.vue";
 import Main from "@/views/Main/Main.vue";
 import Forbidden from "@/views/ErrorPage/403.vue";
 import NotFound from "@/views/ErrorPage/404.vue";
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/login",
--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -6,10 +6,10 @@ if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
   //如果当前环境为生产环境
-  BASE_URL = 'http://coderwhy.org/prod'
+  BASE_URL = '/backend/'
 } else {
   //如果都不是两者
-  BASE_URL = 'http://coderwhy.org/test'
+  BASE_URL = 'http://123.207.32.32:5000'
 }
 
 export { BASE_URL, TIME_OUT }
